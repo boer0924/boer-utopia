c03e29c718c4872ce72f3a2e215739272f71c773
boer <boer0924@qq.com>
2016-07-08 15:31:45 +0800

    modify pre page blog num

--- config.py
+++ config.py
@@ -20,8 +20,8 @@ class ProductionConfig(BaseConfig):
     CSRF_ENABLED = True
     SECRET_KEY = 'C6\x1dO\xc9\xb1\x92Vd\xc4\xc9\xb5\xe18\x97]\x85\xe1\xa9\x100G\x9f\xbf'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    POSTS_PER_PAGE = 10
-    HOT_POSTS_COUNT = 4
+    POSTS_PER_PAGE = 16
+    HOT_POSTS_COUNT = 6
 ## system env
 # export APP_SETTINGS="config.DevelopmentConfig"
 # heroku config:set APP_SETTINGS="config.ProductionConfig"
