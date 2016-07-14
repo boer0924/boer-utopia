17f72ddb72d5bdeb7ddaaf21eea798271b2c2d0c
boer <boer0924@qq.com>
2016-07-14 15:44:47 +0800

    db_create

--- db_create.py
+++ db_create.py
@@ -6,10 +6,11 @@ import os.path
 
 SQLALCHEMY_MIGRATE_REPO = app.config['SQLALCHEMY_MIGRATE_REPO']
 SQLALCHEMY_DATABASE_URI = app.config['SQLALCHEMY_DATABASE_URI']
-# db.create_all()
+db.create_all()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 else:
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO,
                         api.version(SQLALCHEMY_MIGRATE_REPO))
+print '/'.join([basedir, database]), green_text('was created!!!')
